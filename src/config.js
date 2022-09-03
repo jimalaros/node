7ef66063a8520b52467ec4b1e26e6bcc70262c82
jimalaros <jimalaros25@gmail.com>
2022-09-03 16:00:05 -0500

    first commit

--- src/config.js
+++ src/config.js
@@ -2,7 +2,12 @@ require('dotenv').config()
 
 const config = {
     port: process.env.PORT,
-    mongoDbUri: process.env.MONGODB_URI,
+    dbUser: process.env.USER,
+    dbPassword: process.env.PASSWORD,
+    dbHost:process.env.HOST,
+    dbHost1:process.env.HOST1,
+    dbHost2:process.env.HOST2,
+    dbPort:process.env.PORTDB,
     jwtSecret: process.env.JWT_SECRET
 }
 
--- src/database.js
+++ src/database.js
@@ -1,10 +1,16 @@
 const mongoose = require('mongoose')
-const { mongoDbUri } = require('./config')
+const { dbUser, dbPassword, dbHost, dbHost1, dbHost2, dbPort } = require('./config')
 
 const connection = async () => {
-    const conn = await mongoose.connect(mongoDbUri)
-    // eslint-disable-next-line no-console
-    console.log('Mongo DB connected:', conn.connection.name)
+    try {
+        const conn = await mongoose.connect(
+            `mongodb://${dbUser}:${dbPassword}@${dbHost}:${dbPort},${dbHost1}:27017,${dbHost2}:${dbPort}/?ssl=true&replicaSet=atlas-8hcdp9-shard-0&authSource=admin&retryWrites=true&w=majority`
+        )
+        console.log('Mongo DB connected:', conn.connection.name)
+    } catch (error) {
+        console.log(error);
+    }
+    
 }
 
 module.exports = { connection }
