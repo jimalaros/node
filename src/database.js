0bf6fc757abfb668ec6d255a69d3b479040bc911
jimalaros <jimalaros25@gmail.com>
2022-09-03 18:44:45 -0500

    first commit

--- src/database.js
+++ src/database.js
@@ -3,7 +3,6 @@ const { mongoDbUri } = require('./config')
 
 const connection = async () => {
     const conn = await mongoose.connect(mongoDbUri)
-    // eslint-disable-next-line no-console
     console.log('Mongo DB connected:', conn.connection.name)
 }
 
